b5bfa7db3beec1f574f236c44d5b57cdca87d757
Meggan <meggtriplett@gmail.com>
2019-06-14 13:58:23 -0700

    Base method scaffolding

--- Word Guess Game/Word Guess Game/Program.cs
+++ Word Guess Game/Word Guess Game/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Word_Guess_Game
 {
@@ -8,5 +9,71 @@ namespace Word_Guess_Game
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void HomeNavigation()
+        {
+
+        }
+
+        static void CreateWordFile(string filePath)
+        {
+
+        }
+
+        static void AddWordToFile(string word)
+        {
+
+        }
+
+        static void RemoveWordFromFile(string word)
+        {
+
+        }
+
+        static void ExitGame()
+        {
+
+        }
+
+        static void StartGame()
+        {
+
+        }
+
+        static string SelectRandomWordFromFile(string filePath)
+        {
+
+        }
+
+        static char[] PrepareWordForGame(string randomWordFromFile)
+        {
+
+        }
+
+        static char[] LetterGuessList(char letterGuess)
+        {
+
+        }
+
+        static void GameCompleteOptions()
+        {
+
+        }
+
+        static char[] WordDisplay(char[] blankWordDisplay, char LetterGuess)
+        {
+
+        }
+
+        static char[] BlankWordDisplay(char[] preparedWordForGame)
+        {
+
+        }
+
+        static void ExceptionHandler()
+        {
+
+        }
+
     }
 }
